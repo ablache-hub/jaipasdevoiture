69bbc47b7c61307c282ec385a8a03205813fbec9
manulep <emmanuel.lepevedic.pro@gmail.com>
2020-10-15 12:52:59 +0200

    Agence

---
+++ src/main/java/co/simplon/Agence.java
@@ -0,0 +1,36 @@
+package co.simplon;
+
+import java.util.ArrayList;
+
+public class Agence {
+    String ville;
+    ArrayList<Voiture> stock;
+
+    public Agence (String ville) {
+        this.ville = ville;
+        this.stock = new ArrayList<Voiture>();
+    }
+
+    void ajouterVoitureDansStock(Voiture voiture) {
+        this.stock.add(voiture);
+        System.out.println(voiture.marque + " " + voiture.couleur + " ajouté");
+    }
+
+    void afficherStock(){
+        for (Voiture voiture : stock) {
+            System.out.println(voiture.getChaine());
+        }
+    }
+
+    String getChaine(){
+        return "Agence de " + this.ville;
+    }
+
+    void rentrerDuStock(){
+        Voiture renault = new Voiture("Renault","noire");
+       // System.out.println(renault.getChaine());
+        Voiture ferrari = new Voiture("Ferrari", "rouge");
+        this.ajouterVoitureDansStock(renault);
+        this.ajouterVoitureDansStock(ferrari);
+    }
+}
