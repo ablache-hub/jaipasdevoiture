278f08b2f282f9c7d7573dfb87d6aa59eefcc436
manulep <emmanuel.lepevedic.pro@gmail.com>
2020-10-15 12:52:51 +0200

    Voiture

---
+++ src/main/java/co/simplon/Voiture.java
@@ -0,0 +1,23 @@
+package co.simplon
+;
+
+/**
+ * Voiture
+ */
+public class Voiture {
+    String marque;
+    String couleur;
+    int niveauEssence;
+    boolean reservoireVide;
+    boolean reservee;
+    
+    public Voiture (String marque, String couleur) {
+        this.marque = marque;
+        this.couleur = couleur;
+        this.reservee = false;
+    }
+
+    public String getChaine() {
+        return "marque: " + this.marque + ", couleur: "+ this.couleur;
+    }
+}
